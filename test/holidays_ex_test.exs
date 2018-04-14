3980f9f30ff949e37ab5aa814671204ec121d9b6
inokappa <inokara@gmail.com>
2018-04-14 12:52:21 +0900

    fix test

--- test/holidays_ex_test.exs
+++ test/holidays_ex_test.exs
@@ -3,15 +3,14 @@ defmodule HolidaysExTest do
   doctest HolidaysEx, except: [:moduledoc, all: 0, to_json: 0]
 
   setup_all do
-    {:ok, days: ["2017-07-17", "2018-07-16", "2019-07-15"]}
-    {:ok, holiday: "元日"}
+    {:ok, days: ["2017-07-17", "2018-07-16", "2019-07-15"], holiday: "元日"}
   end
 
   test "Get Umino-Hi's dates", state do
     assert HolidaysEx.when?("海の日") == state[:days]
   end
 
-  test "Get holiday's name by date" do
+  test "Get holiday's name by date", state do
     assert HolidaysEx.holiday?("2018-01-01") == state[:holiday]
   end
 end
