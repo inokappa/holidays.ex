842282f57f3fd449aeb1dbb5772aab7e61a53116
inokappa <inokara@gmail.com>
2018-04-14 23:38:43 +0900

    add Mock

--- test/holidays_ex_test.exs
+++ test/holidays_ex_test.exs
@@ -1,16 +1,36 @@
 defmodule HolidaysExTest do
   use ExUnit.Case
-  doctest HolidaysEx, except: [:moduledoc, all: 0, to_json: 0]
+  doctest HolidaysEx, except: [:moduledoc, all: 0,
+                                           dates: 0,
+                                           to_json: 0
+                              ]
+
+  import Mock
 
   setup_all do
-    {:ok, days: ["2017-07-17", "2018-07-16", "2019-07-15"], holiday: "元日"}
+    {:ok, days: ["2017-07-17", "2018-07-16", "2019-07-15"],
+          holiday: "元日"
+    }
   end
 
-  test "Get Umino-Hi's dates", state do
-    assert HolidaysEx.when?("海の日") == state[:days]
+  test "Return Umino-Hi's dates", state do
+    with_mock HolidaysEx,
+      [when?: fn("海の日") -> state[:days] end] do
+      assert HolidaysEx.when?("海の日") == state[:days]
+    end
   end
 
-  test "Get holiday's name by date", state do
-    assert HolidaysEx.holiday?("2018-01-01") == state[:holiday]
+  test "Return All Holidays(date only)", state do
+    with_mock HolidaysEx,
+      [dates: fn() -> state[:days] end] do
+      assert HolidaysEx.dates() == state[:days]
+    end
+  end
+
+  test "Return holiday's name by date", state do
+    with_mock HolidaysEx,
+      [holiday?: fn("2018-01-01") -> state[:holiday] end] do
+      assert HolidaysEx.holiday?("2018-01-01") == state[:holiday]
+    end
   end
 end
